539292dae21dd1df0a358406dcb3197c966b0d44
Ananya212 <annroy.212@gmail.com>
2015-08-19 14:27:13 +0530

    I have edited

--- calci.cpp
+++ calci.cpp
@@ -3,8 +3,10 @@ using namespace std;
 int main()
 {
 int a,b,c;
-a=5;
-b=8;
+cout<<"enter value of a and b";
+cin>>a>>b;
+cout<<"value of a is"<<a;
+cout<<"value of b is"<<b;
 c=a+b;
 cout<<"End value is"<<c;
 return 0;
