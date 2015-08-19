361d4644cbacef100552f321e41cf67b7279c497
Ananya212 <annroy.212@gmail.com>
2015-08-19 14:32:14 +0530

    3rd

--- calci.cpp
+++ calci.cpp
@@ -6,6 +6,10 @@ int a,b,c;
 cout<<"enter value of a and b";
 cin>>a>>b;
 cout<<"value of a is"<<a;
+if (a<0||b<0)
+{
+  cout<<"enter valid value";
+}
 cout<<"value of b is"<<b;
 c=a+b;
 cout<<"End value is"<<c;
